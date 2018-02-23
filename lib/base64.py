323ed3ee1d92a28887e1b29c060b32ffea4a625a
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-02-23 13:00:02 +0200

    viikkorapsa ja coverage

--- lib/base64.py
+++ lib/base64.py
@@ -3,27 +3,28 @@ class Base64:
     """
     https://tools.ietf.org/html/rfc4648
     """
+
     def __init__(self):
         self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-        
+
     def encode(self, data):
         if len(data) == 0:
-            return ""  
+            return ""
 
         out = ""
         npad = len(data) % 3
 
         if npad > 0:
             data += '\0' * (3-npad)
-      
+
         for i in xrange(0, len(data), 3):
             enc = (ord(data[i]) << 16) + (ord(data[i+1]) << 8) + ord(data[i+2])
-            
+
             out += self.alphabet[(enc >> 18) & 63]
             out += self.alphabet[(enc >> 12) & 63]
             out += self.alphabet[(enc >> 6) & 63]
             out += self.alphabet[enc & 63]
-        
+
         if npad > 0:
             out = out[:-(3-npad)] + '=' * (3-npad)
 
@@ -31,7 +32,7 @@ class Base64:
 
     def decode(self, data):
         out = ""
-        
+
         npad = data.count('=')
         data = data.replace('=', 'A')
 
@@ -39,16 +40,13 @@ class Base64:
             ch = (self.alphabet.index(data[i]) << 18) + \
                  (self.alphabet.index(data[i+1]) << 12) + \
                  (self.alphabet.index(data[i+2]) << 6) + \
-                  self.alphabet.index(data[i+3])
-            
+                self.alphabet.index(data[i+3])
+
             out += chr((ch >> 16) & 255)
             out += chr((ch >> 8) & 255)
             out += chr(ch & 255)
-        
+
         if npad > 0:
             out = out[:-npad]
-            
-        return out
-            
-    
 
+        return out
---
+++ viikkoraportit/viikko6.md
@@ -0,0 +1,13 @@
+# Viikko 6
+
+Sain nyt demo ui:n tehtyä ja fiksailtua vähän bugeja. 
+UI on aika simppeli React-häkkyrä joka vain käyttää noden HTTP API:a. En
+sille siis mitään testejä tehnyt. Lisään sinne vielä mikäli vain ehdin, niin tuon blockchainin
+selaamisen. Tämä lienee riittävä laajuus nyt ainakin ja aika monta erilaista algoritmia
+on tullut tehtyä tähän. Katselen vielä onko tuolla jotain tietorakennetta / algoritmia joka 
+olisi syytä korvata omalla ja kirjoittelen lähipäivinä dokkarit. 
+
+
+
+
+
